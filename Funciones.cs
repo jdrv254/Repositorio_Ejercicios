293c27b84cbc8bb493d6c7179bde00d9eb1112c0
jdrv254 <126375471+jdrv254@users.noreply.github.com>
2023-03-09 19:41:52 -0500

    Funciones
    Funciones

---
+++ Funciones.cs
@@ -0,0 +1,37 @@
+﻿// void WriteLine(string Message){
+//     Console.WriteLine(Message);
+// }
+
+// WriteLine("Hola");
+
+// int sumemelo(int x,int y){
+//         return x+y;
+// }
+
+// Console.WriteLine(sumemelo(3,4));
+
+// double dividamelo(int x,int y){
+//         if (y == 0)
+//         {
+//             throw new ArgumentException($"No se puede dividir por el el numero {y}");
+//         }
+//         return x/y;
+// }
+
+// Console.WriteLine(dividamelo(1,0));
+
+int[] Numero = new int[]{1,2,3,4,5,6,7,8,9,10};
+
+int NumberPares(int[] k ){
+
+    int aux =0;
+    for (int i = 0; i < k.Length; i++)
+    {
+        if (k[i] % 2 == 0) aux++; 
+    }
+
+    return aux;
+}
+
+
+NumberPares(Numero);
